39764955cd272daf74943e07ac47656292f65c60
xiaotian ding <xiaotian.ding.gms@gmail.com>
2014-09-21 20:06:02 -0700

    temporary save

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -14,4 +14,17 @@
 /Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/res/values/strings.xml \
 /Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/res/xml/config.xml \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable/icon.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-land-mdpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-port-mdpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
+/Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/screenrewards/Documents/workspace/UDPcordova_Client/platforms/android/ant-build/AndroidManifest.xml \
